f086a421032e7e44c2d33b465a2e9b6283b88dea
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-01-14 13:19:39 -0300

    working on domain class User with utility types, interfaces and types

--- src/modules/user/domain/user.ts
+++ src/modules/user/domain/user.ts
@@ -1,3 +1,28 @@
+// INTERFACES
+interface UserRequired {
+  id: number
+  name: string
+  lastname: string
+  email: string
+  password: string
+}
+
+interface UserOptional {
+  refreshToken: string
+  active: boolean
+}
+
+type UserUpdate = {
+  name: string
+  lastname: string
+  password: string
+  refreshToken: string
+  active: boolean
+}
+
+export type UserProperties = Required<UserRequired> & Partial<UserOptional>
+
+// MODELO DE DOMINIO
 export default class User {
   private readonly id: number
   private name: string
@@ -5,23 +30,31 @@ export default class User {
   private readonly email: string
   private password: string
   private refreshtoken: string
-  private active: boolean | null
+  private active: boolean
 
-  constructor(
-    id: number,
-    name: string,
-    lastname: string,
-    email: string,
-    password: string,
-    refreshtoken: string,
-    active: boolean,
-  ) {
-    this.id = id
-    this.name = name
-    this.lastname = lastname
-    this.email = email
-    this.password = password
-    this.refreshtoken = refreshtoken
-    this.active = active
+  constructor(userProperties: UserProperties) {
+    this.active = true
+    Object.assign(this, userProperties)
+  }
+
+  // METHODS
+  properties(): UserProperties {
+    return {
+      id: this.id,
+      name: this.name,
+      lastname: this.lastname,
+      email: this.email,
+      password: this.password,
+      refreshToken: this.refreshtoken,
+      active: this.active,
+    }
+  }
+
+  update(fields: UserUpdate) {
+    Object.assign(this, fields)
+  }
+
+  delete() {
+    this.active = false
   }
 }
